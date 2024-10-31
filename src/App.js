dd76477f2523f88b01857e5d736270a163b81682
Bishal <hapiebishal5@gmail.com>
2024-10-31 22:37:09 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Listgroup from './component/Listgroup';
+import Carousel from './component/Carousel';
 
 function App() {
   return (
     <div>
-   <Listgroup/>
+   {/* <Listgroup/> */}
+   <Carousel/>
     </div>
   );
 }
